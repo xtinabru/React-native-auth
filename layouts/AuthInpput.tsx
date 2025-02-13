f093d4500858cbb4b355d6c456d752901b3d2cc5
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-02-13 03:07:29 +0200

    Make the icons

--- layouts/AuthInpput.tsx
+++ layouts/AuthInpput.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { TextInput, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
 interface AuthInputProps {
   placeholder: string;
@@ -8,8 +9,20 @@ interface AuthInputProps {
 }
 
 const AuthInput = ({ placeholder, secureTextEntry, onForgotPassword }: AuthInputProps) => {
+
+  const getIconName = () => {
+    if (placeholder.toLowerCase() === "email") return "mail"; // email icon
+    if (placeholder.toLowerCase() === "password") return "lock";   // password icon
+    return null;
+  };
+
+  
+
   return (
     <View style={styles.inputContainer}>
+    {getIconName() && (
+        <Icon name={getIconName()!} size={20} color="#999" style={styles.icon} />
+      )}
      
       <TextInput
         placeholder={placeholder.toUpperCase()}
@@ -32,21 +45,29 @@ const styles = StyleSheet.create({
     position: 'relative', // to fix the link inside the input
     marginBottom: 20,
   },
+  icon: {
+    position: "absolute",
+    left: 15,  
+    top: "50%",
+    transform: [{ translateY: -10 }], 
+    color: 'black',
+    zIndex: 1, 
+  },
   input: {
     height: 45,
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
-    paddingLeft: 10, 
+    paddingLeft: 40, 
     paddingRight: 60, 
-    fontSize: 16,
+    fontSize: 12,
     backgroundColor: '#fff',
   },
   forgotContainer: {
     position: 'absolute',
-    right: 10,
+    right: 20,
     top: '50%',
-    transform: [{ translateY: -10 }], // put in the mid
+    transform: [{ translateY: -5 }], // put in the mid
   },
   forgotText: {
     fontSize: 12,
